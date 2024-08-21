de76a8a2e0fd86fb22f061bc70078ef02e0b7425
temple_19 <tozoaz@gmail.com>
2024-08-21 22:25:10 +0100

    google1.0

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,44 +1,42 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import LinkedInProvider from "next-auth/providers/linkedin";
 
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
-
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    LinkedInProvider({
-      clientId: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    }),
   ],
   callbacks: {
     async session({ session }) {
       // store the user id from MongoDB to session
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser._id.toString();
+      // const sessionUser = await User.findOne({ email: session.user.email });
+      // session.user.id = sessionUser._id.toString();
 
       return session;
     },
     async signIn({ account, profile, user, credentials }) {
       try {
-        await connectToDB();
+        // await connectToDB();
 
-        // check if user already exists
-        const userExists = await User.findOne({ email: profile.email });
+        // // check if user already exists
+        // const userExists = await User.findOne({ email: profile.email });
 
-        // if not, create a new document and save user in MongoDB
-        if (!userExists) {
-          await User.create({
-            email: profile.email,
-            username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.picture,
-          });
-        }
+        // // if not, create a new document and save user in MongoDB
+        // if (!userExists) {
+        //   await User.create({
+        //     email: profile.email,
+        //     username: profile.name.replace(" ", "").toLowerCase(),
+        //     image: profile.picture,
+        //   });
+        // }
 
         return true;
       } catch (error) {
